dd5ea51f2fdb07792a069dc122b78c4e9db520a1
github daisukenoguchi <daisukenoguemon@gmail.com>
2022-02-12 00:11:27 +0900

    add tracking

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import './assets/tailwind.css'
 import './assets/fontawesome-free/css/all.min.css'
 
+import VueGtag from "vue-gtag"
+
 import VueCompositionApi from '@vue/composition-api';
 Vue.use(VueCompositionApi);
+Vue.use(VueGtag, {
+  config: { id: "G-ZL8HYW9VXV" }
+});
 
 Vue.config.productionTip = false
 
